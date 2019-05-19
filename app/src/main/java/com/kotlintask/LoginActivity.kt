2137dc8e62fec7e5ea2d729e356c6aecce7339b0
AlShaymaa Mohamed <shaymaa908@yahoo.com>
2019-05-19 21:59:45 +0300

    Update Location

--- app/src/main/java/com/kotlintask/LoginActivity.kt
+++ app/src/main/java/com/kotlintask/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,10 +15,15 @@ class LoginActivity : AppCompatActivity() {
         login_button.setOnClickListener{
             val userName=user_name_edit_text.text.toString()
             val password=password_edit_text.text.toString()
-            val intent = Intent(applicationContext, LocationActivity::class.java)
-            intent.putExtra("userName",userName)
-            intent.putExtra("password",password)
-            startActivity(intent)
+            if(!userName.isEmpty()&&!password.isEmpty()){
+                val intent = Intent(applicationContext, LocationActivity::class.java)
+                intent.putExtra("userName",userName)
+                intent.putExtra("password",password)
+                startActivity(intent)
+            }else{
+                Toast.makeText(applicationContext,"Enter Valid Name and Password",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
